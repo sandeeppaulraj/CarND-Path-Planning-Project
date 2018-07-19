5c9479db2e2e5233359dba57773697d5ff5c78ce
sandeeppaulraj <sandeeppaulraj@gmail.com>
2018-07-18 22:13:55 -0700

    fix build error

--- src/main.cpp
+++ src/main.cpp
@@ -247,8 +247,8 @@ int main() {
 			double dist_inc = 0.5;
 			for (int i=0; i < 50; i++)
 			{
-				next_x_vals.push_back(car_x + (dist_inc*i)*cos(deg2rad(car_yaw)))
-				next_y_vals.push_back(car_y + (dist_inc*i)*sin(deg2rad(car_yaw)))
+				next_x_vals.push_back(car_x + (dist_inc*i)*cos(deg2rad(car_yaw)));
+				next_y_vals.push_back(car_y + (dist_inc*i)*sin(deg2rad(car_yaw)));
 			}
 
           	msgJson["next_x"] = next_x_vals;
