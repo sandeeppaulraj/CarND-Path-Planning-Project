c04190a963fc47fffa36e824e2e03726850d313d
sandeeppaulraj <sandeeppaulraj@gmail.com>
2018-07-19 00:35:49 -0700

    build issue

--- src/main.cpp
+++ src/main.cpp
@@ -280,6 +280,8 @@ int main() {
 				
 			}
 			
+			lane = 1;
+			
 			vector<double> next_wp0 = getXY(car_s + 30, (2 + 4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
 			vector<double> next_wp1 = getXY(car_s + 60, (2 + 4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
 			vector<double> next_wp2 = getXY(car_s + 90, (2 + 4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
